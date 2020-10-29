9f14f2c162e3df8d6c3e924bf373ce5c91f4fc35
Alec Coates <42540348+AlecCoates@users.noreply.github.com>
2020-10-29 14:05:19 +0000

    Added initial game code
    Added the initial libgdx game project file

---
+++ BoatRace2020/desktop/src/com/quackthulu/boatrace2020/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.quackthulu.boatrace2020.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.quackthulu.boatrace2020.BoatRace;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BoatRace(), config);
+	}
+}
