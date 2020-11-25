b26153626918afe152d29d4ea7c18784174681fd
Alec Coates <42540348+AlecCoates@users.noreply.github.com>
2020-11-25 02:05:45 +0000

    Update GameScreen.java

--- BoatRace2020/core/src/com/quackthulu/boatrace2020/GameScreen.java
+++ BoatRace2020/core/src/com/quackthulu/boatrace2020/GameScreen.java
@@ -37,7 +37,7 @@ public class GameScreen implements Screen {
     private float laneWidthsScreen = 6.5f;
     private float minAspectRatio = 1.8f;
     private float boatWidthsLane = 4.0f;
-    private float raceLength = 5.0f;
+    private float raceLength = 40.0f;
     private int backgroundTextureSize = 64;
     private float riverCountX = 0;
     private float riverCountY = 0;
