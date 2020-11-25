1433f2ea64237cf470cd849c536df0a76532ce21
Alec Coates <42540348+AlecCoates@users.noreply.github.com>
2020-11-25 01:40:17 +0000

    Fullscreen

--- BoatRace2020/core/src/com/quackthulu/boatrace2020/BoatRace.java
+++ BoatRace2020/core/src/com/quackthulu/boatrace2020/BoatRace.java
@@ -46,7 +46,7 @@ public class BoatRace extends Game {
 		LOSE_SCREEN = new Lose(this);
 		WIN_SCREEN = new Win(this);
 		PICK_BOAT_SCREEN = new PickBoatMenu(this);
-		//Gdx.graphics.setFullscreenMode(Gdx.graphics.getDisplayMode());
+		Gdx.graphics.setFullscreenMode(Gdx.graphics.getDisplayMode());
 		setScreen(MENU_SCREEN);
 
 		settings = new Settings();
